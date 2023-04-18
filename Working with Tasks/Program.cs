4feca5cd38d07839a8b509ba04c8c91ce2b4efce
Rayna Miller <89815185+UsernameI23@users.noreply.github.com>
2023-04-18 17:32:18 -0400

    Update Program.cs

--- Working with Tasks/Program.cs
+++ Working with Tasks/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace threadRace
+namespace threadRace
 {
     internal class Program
     {
@@ -12,7 +12,7 @@
             int t5Location = 0;
 
             //Creating Threads
-            Thread t1 = new Thread(delegate ()
+            Task t1 = Task.Run(() =>
             {
                 Thread.CurrentThread.Name = "Speedy Gonzales";
                 for (int i = 0; i < 100; i++)
@@ -22,53 +22,59 @@
                 }
 
             });
-            Thread t2 = new Thread(delegate ()
+            Task t2 = Task.Run(() =>
             {
                 Thread.CurrentThread.Name = "Road Runner";
                 for (int i = 0; i < 100; i++)
                 {
                     if (t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
-                        MoveIt(ref t2Location);
-
+                        MoveIt(ref t1Location);
                 }
+
             });
-            Thread t3 = new Thread(delegate ()
+            Task t3 = Task.Run(() =>
             {
                 Thread.CurrentThread.Name = "Bugs Bunny";
                 for (int i = 0; i < 100; i++)
                 {
                     if (t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
-                        MoveIt(ref t2Location);
-
+                        MoveIt(ref t1Location);
                 }
+
             });
-            Thread t4 = new Thread(delegate ()
+            Task t4 = Task.Run(() =>
             {
                 Thread.CurrentThread.Name = "Daffy Duck";
                 for (int i = 0; i < 100; i++)
                 {
                     if (t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
-                        MoveIt(ref t2Location);
-
+                        MoveIt(ref t1Location);
                 }
+
             });
-            Thread t5 = new Thread(delegate ()
+            Task t5 = Task.Run(() =>
             {
                 Thread.CurrentThread.Name = "Porky Pig";
                 for (int i = 0; i < 100; i++)
                 {
                     if (t1Location < 100 && t2Location < 100 && t3Location < 100 && t4Location < 100 && t5Location < 100)
-                        MoveIt(ref t2Location);
-
+                        MoveIt(ref t1Location);
                 }
+
             });
+            //t3.Priority = ThreadPriority.AboveNormal;
 
             //Executing the methods
-            t1.Start();
-            t2.Start();
-            t3.Start();
-            t4.Start();
-            t5.Start();
+            //t1.Start();
+            //t2.Start();
+            //t3.Start();
+            //t4.Start();
+            //t5.Start();
+            Task.WaitAny(t1);
+            Task.WaitAny(t2);
+            Task.WaitAny(t3);
+            Task.WaitAny(t4);
+            Task.WaitAny(t5);
             Console.WriteLine("Race has ended");
         }
         static void MoveIt(ref int location)
@@ -85,3 +91,4 @@
 
     }
 }
+
